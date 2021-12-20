6a23f6cfb6ca8ff9925bfa013b798119c54c7d42
Harutyunyan Gevorg <gev.harutyunyan.96@gmail.com>
2021-12-20 22:25:07 +0400

    feat: working codes

---
+++ Task4.js
@@ -0,0 +1,35 @@
+function gradeLab(studentLab){
+    try{
+    for(let obj of studentLab){
+    let {student, runLab} = obj;
+    if(runLab === undefined || runLab(3,3) !== Math.pow(3,3)){
+        console.log(`${student} code working false`);
+    }else{
+        console.log(`${student} code working true`);
+    }
+}
+    
+}catch(err){
+    console.log(err.message);
+}
+}
+gradeLab([
+    {
+    student: "Blake",
+    myCode: function (num) {
+    return Math.pow(num, num);
+    },
+    },
+    {
+    student: "Jessica",
+    runLab: function (num) {
+    return Math.pow(num, num);
+    },
+    },
+    {
+    student: "Mya",
+    runLab: function (num) {
+    return num * num;
+    },
+    },
+    ])
