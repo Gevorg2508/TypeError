2cc002860426250377ef7f79540f5d94de2b3146
Harutyunyan Gevorg <gev.harutyunyan.96@gmail.com>
2021-12-20 20:40:35 +0400

    feat: split reverse join

---
+++ Task1.js
@@ -0,0 +1,11 @@
+let str = 'forest'
+try {
+    
+function reverse(str){
+    return console.log(str.split('').reverse().join(''));
+}
+reverse("hello");
+}catch(err){
+    console.log(err.message);
+}
+
